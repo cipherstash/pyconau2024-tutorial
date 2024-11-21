fedd08001f1548058b721cf71d97582b013021ba
Dan Draper <dan@cipherstash.com>
2024-11-21 20:55:10 +1100

    Set padding to pkcs5

--- models.py
+++ models.py
@@ -16,7 +16,7 @@ class User(Base):
     id = Column(Integer, primary_key=True)
     name = Column(String(50), unique=False)
     email = Column(String(120), unique=True)
-    secret = Column(StringEncryptedType(String, length=255, key='abc'))
+    secret = Column(StringEncryptedType(String, length=255, key='abc', padding='pkcs5'))
     gender = Column(StringEncryptedType(String, engine=AesEngine, length=255, key='abc'))
     safer_gender = Column(StringEncryptedType(String, engine=AesGcmEngine, length=255, key='abc'))
 
